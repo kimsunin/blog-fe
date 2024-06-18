712644b414a4f8afd215c237ae0a2c3ec0cb542e
kimsunin <suninkim10@gmail.com>
2024-06-18 14:18:55 +0900

    -feat/MarkdownView: 태그이름으로 작성시 태그이름이 그대로 노출되는 부분 수정
    - rehypePlugins={[rehypeRaw]}를 통한 대응

--- src/common/components/MarkDownView/MarkDownView.tsx
+++ src/common/components/MarkDownView/MarkDownView.tsx
@@ -1,4 +1,5 @@
 import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
 import styles from "./MarkDownView.module.css";
 
 type PropsType = {
@@ -6,7 +7,11 @@ type PropsType = {
 };
 
 function MarkDownView({ post }: PropsType) {
-  return <ReactMarkdown className={styles.markdownview}>{post}</ReactMarkdown>;
+  return (
+    <ReactMarkdown className={styles.markdownview} rehypePlugins={[rehypeRaw]}>
+      {post}
+    </ReactMarkdown>
+  );
 }
 
 export default MarkDownView;
