634dbbc1e09b867a19f93d8603ef524b887515af
kimsunin <suninkim10@gmail.com>
2024-09-11 01:15:38 +0900

    feat:update navHref

--- src/models/navHref.tsx
+++ src/models/navHref.tsx
@@ -1,5 +1,4 @@
 const navHref = [
-  { title: "Home", href: "/", comment: "집" },
   { title: "Deve", href: "/deve", comment: "개발에 관한 것들을 기록하는 공간" },
   { title: "Note", href: "/note", comment: "자유롭게 기록하고 기억하는 공간" },
   { title: "Craf", href: "/craf", comment: "직접 만들고 보여주는 공간" },
