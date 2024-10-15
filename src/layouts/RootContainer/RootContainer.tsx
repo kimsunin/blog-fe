b1d18b054cdf3091601782b5accc26411082ac17
kimsunin <suninkim10@gmail.com>
2024-10-15 15:56:00 +0900

    feat:바로가기 to kimsi.me

--- src/layouts/RootContainer/RootContainer.tsx
+++ src/layouts/RootContainer/RootContainer.tsx
@@ -1,6 +1,8 @@
 import Header from "@/layouts/Header/Header";
 import styles from "./RootContainer.module.css";
 import Nav from "@/layouts/Nav/Nav";
+import Link from "next/link";
+import CustomLink from "@/components/CustomLink/CustomLink";
 
 function RootContainer({
   children,
@@ -9,6 +11,7 @@ function RootContainer({
 }>) {
   return (
     <div className={styles.root_container}>
+      <CustomLink href={"https://kimsi.me"} label="블로그가 이전되었습니다" />
       <Header />
       <Nav />
       {children}
